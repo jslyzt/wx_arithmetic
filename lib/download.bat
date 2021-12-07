d07aa1e9381e7dccb281c59b940e1dbe104b3219
jslyzt <tao.zhou01@yingxiong.com>
2021-12-07 16:00:00 +0800

    wxWidgets 3.1.4 => 3.1.5

--- lib/download.bat
+++ lib/download.bat
@@ -1,14 +1,14 @@
 @echo off
 set PATH=%PATH%;tool
 
-if not exist wxWidgets-3.1.4-headers.7z (
-   wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.4/wxWidgets-3.1.4-headers.7z
+if not exist wxWidgets-3.1.5-headers.7z (
+   wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.5/wxWidgets-3.1.5-headers.7z
 )
 
-if not exist wxMSW-3.1.4_vc14x_x64_Dev.7z (
-   wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.4/wxMSW-3.1.4_vc14x_x64_Dev.7z
+if not exist wxMSW-3.1.5_vc14x_x64_Dev.7z (
+   wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.5/wxMSW-3.1.5_vc14x_x64_Dev.7z
 )
 
 mkdir wxwidgets
-7z x wxWidgets-3.1.4-headers.7z -owxwidgets
-7z x wxMSW-3.1.4_vc14x_x64_Dev.7z -owxwidgets
+7z x wxWidgets-3.1.5-headers.7z -owxwidgets
+7z x wxMSW-3.1.5_vc14x_x64_Dev.7z -owxwidgets
